40e308fb5bbbecc44058a0a687d3016f84066f08
Eric Fei <guanwha@gmail.com>
2021-11-17 17:55:16 +0800

    Recover the router base & removed lazy-loading & build gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import SearchScenicSpots from '../views/SearchScenicSpots.vue';
+import ScenicSpot from '../views/ScenicSpot.vue';
 import { MenuItemTypeEnum } from '../utils/enums';
 
 Vue.use(VueRouter);
@@ -20,7 +22,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "searchScenicSpots" */ '../views/SearchScenicSpots.vue'),
+    // component: () => import(/* webpackChunkName: "searchScenicSpots" */ '../views/SearchScenicSpots.vue'),
+    component: SearchScenicSpots,
     meta: {
       menuItemType: MenuItemTypeEnum.SEARCH_SCENICSPOTS,
     },
@@ -28,7 +31,8 @@ const routes = [
   {
     path: '/scenicspot/:id',
     name: 'scenicSpot',
-    component: () => import(/* webpackChunkName: "searchScenicSpots" */ '../views/ScenicSpot.vue'),
+    // component: () => import(/* webpackChunkName: "searchScenicSpots" */ '../views/ScenicSpot.vue'),
+    component: ScenicSpot,
     meta: {
       menuItemType: MenuItemTypeEnum.SEARCH_SCENICSPOTS,
     },
@@ -37,7 +41,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: '/',
+  base: process.env.BASE_URL,
   routes,
 });
 
